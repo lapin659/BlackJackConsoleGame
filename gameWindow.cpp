615b201a8acb03b6efbed0f1c83b8dd0e601996e
Tyrone <tforte190@gmail.com>
2021-04-12 21:19:15 -0700

    Adding the project files as of 4/12

---
+++ gameWindow.cpp
@@ -0,0 +1,17 @@
+#include "gameWindow.h"
+
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+
+
+[STAThread]
+
+void main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	blackJack::gameWindow window;
+	Application::Run(% window);
+}
